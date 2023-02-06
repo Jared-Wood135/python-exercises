23f1563a275b4598bf663f12100a4213e6ebe4dd
Jared Wood <jared.wood135@gmail.com>
2023-02-06 13:51:37 -0600

    Question template completed, questions partially answered, will continue

---
+++ python_introduction_exercises.py
@@ -0,0 +1,23 @@
+# 5. Create a script named python_introduction_exercises.py that prints "Hello, World!" to the console.
+print('Hello, World!')
+
+# 6. Run this program from the command line.
+# INPUT IN CLI: python python_introduction_exercises.py
+
+# 7. Modify the program to contain a variable named greeting that holds the message that you will 
+# print to the console.
+greeting = 'Hello, World!'
+
+# 8. Run your script interactively.
+# INPUT IN CLI: python -i python_introduction_exercises.py
+
+# 9. View the contents of the greeting variable.
+
+# 10. Create a Jupyter notebook named my_first_notebook.
+
+# 11. Execute at least 3 Python cells and make some notes that describe 3 keyboard shortcuts 
+# for Jupyter notebooks.
+
+# 12. Use pip to install the pydataset library which contains datasets we will use.
+
+# 13. Add, commit, and push your work so far.
