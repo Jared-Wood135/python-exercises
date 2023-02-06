66546238253ed6966a8262564ea03c806dbc8904
Jared Wood <jared.wood135@gmail.com>
2023-02-06 14:25:45 -0600

    Completed template for later use

---
+++ data_types_and_variables.py
@@ -0,0 +1,102 @@
+# =======================================================================================================
+# Initial 4 Questions START
+# =======================================================================================================
+
+# 1. Identify the data type of the following values:
+# 99.9
+# "False"
+# False
+# '0'
+# 0
+# True
+# 'True'
+# [{}]
+# {'a': []}
+
+# 2. What data type would best represent the following?
+# A term or phrase typed into a search box
+# Whether or not a user is logged in
+# A discount amount to apply to a user's shopping cart
+# Whether or not a coupon code is valid
+# An email address typed into a registration form
+# The price of a product
+# The email addresses collected from a registration form
+# Information about applicants to Codeup's data science program
+
+# 3. For each of the following code blocks:
+# -- Read the expression and predict the evaluated results
+# -- Execute the expression in a Python REPL.
+# '1' + 2
+# 6 % 4
+# type(6 % 4)
+# type(type(6 % 4))
+# '3 + 4 is ' + 3 + 4
+# 0 < 0
+# 'False' == False
+# True == 'True'
+# 5 >= -5
+# True or "42"
+# 6 % 5
+# 5 < 4 and 1 == 1
+# 'codeup' == 'codeup' and 'codeup' == 'Codeup'
+# 4 >= 0 and 1 !== '1'
+# 6 % 3 == 0
+# 5 % 2 != 0
+# [1] + 2
+# [1] + [2]
+# [1] * 2
+# [1] * [2]
+# [] + [] == []
+# {} + {}
+
+# 4. Create a Python script file named data_types_and_variables.py. Inside the script, write 
+#    Python code to describe the following scenarios. You will need to create and assign 
+#    variables and use operators.
+
+# =======================================================================================================
+# Initial 4 Questions END
+# Initial 4 Questions TO Questions 5-9
+# Questions 5-9 START
+# =======================================================================================================
+
+# 5. You have rented some movies for your kids:
+# -- The Little Mermaid for 3 days
+# -- Brother Bear for 5 days
+# -- Hercules for 1 day
+# -- If the daily fee to rent a movie is 3 dollars, how much will you have to pay?
+
+# 6. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook.
+# -- They pay you the following hourly rates:
+# -- Google: 400 dollars
+# -- Amazon: 380 dollars
+# -- Facebook: 350 dollars
+# -- This week you worked: 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+# -- How much will you receive in payment for this week?
+
+# 7. A student can be enrolled to a class only if the class is not full and the class schedule does 
+#    not conflict with her current schedule.
+
+# 8. A product offer can be applied only if people buys more than 2 items, and the offer has not 
+#    expired. Premium members do not need to buy a specific amount of products.
+
+# 9. Continue working in the data_types_and_variables.py file. Use the following code to follow the 
+#    instructions below:
+# -- username = 'codeup'
+# -- password = 'notastrongpassword'
+# -- Create a variable that holds a boolean value for each of the following conditions:
+# -- The password must be at least 5 characters
+# -- The username must be no more than 20 characters
+# -- The password must not be the same as the username
+# -- Bonus Neither the username or password can start or end with whitespace
+
+# =======================================================================================================
+# Questions 5-9 END
+# Questions 5-9 TO Bonus Exercises
+# Bonus Exercises START
+# =======================================================================================================
+
+# 1. For practicing with list comprehensions, work through 17 List Comprehension Exercises. Add your 
+#    solutions to a new file named list_comprehensions.py
+
+# 2. For even more practice with all your Python tools together, work through 20 Python Data Structure 
+#    Manipulation Exercises. Name this file python_data_structure_manipulation_exercises.py.
